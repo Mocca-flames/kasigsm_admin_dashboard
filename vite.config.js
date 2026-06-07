30a8b99d136a4b2aedc9ce34a929e2ce9a13f7e9
Maurice <juniorflamebet@gmail.com>
2026-06-07 18:52:38 +0200

    Vite as Standard

--- vite.config.js
+++ vite.config.js
@@ -4,23 +4,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    proxy: {
-      '/admin': {
-        target: 'http://api.kasigsm.co.za:8000',
-        changeOrigin: true,
-        secure: false,
-      },
-      '/auth': {
-        target: 'http://api.kasigsm.co.za:8000',
-        changeOrigin: true,
-        secure: false,
-      },
-      '/wallet': {
-        target: 'http://api.kasigsm.co.za:8000',
-        changeOrigin: true,
-        secure: false,
-      }
-    }
+    port: 3000,
+    open: false,
   },
   build: {
     chunkSizeWarningLimit: 2000,
